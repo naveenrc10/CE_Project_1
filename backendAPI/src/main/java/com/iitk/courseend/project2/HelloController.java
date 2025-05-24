ad381167626e5d96d5b32db1e28eb3da24c81242
Naveen R C <nav.chakravarthy@hgmail.com>
2025-05-24 06:27:52 +0000

    wip

---
+++ backendAPI/src/main/java/com/iitk/courseend/project2/HelloController.java
@@ -0,0 +1,13 @@
+package com.iitk.courseend.project2;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+    
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, Spring Boot!";
+    }
+}
