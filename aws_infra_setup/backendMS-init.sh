07ae47ad47b9b6be38381c2221864b1d2dceb79a
Naveen R C <nav.chakravarthy@hgmail.com>
2025-05-31 06:17:17 +0000

    www

--- aws_infra_setup/backendMS-init.sh
+++ aws_infra_setup/backendMS-init.sh
@@ -3,11 +3,14 @@
 sudo apt-get update -y
 sudo apt-get install -y ansible unzip
 
-sudo snap install aws-cli --classic
-sudo snap install docker --classic
+sudo apt install docker.io -y
 
+# Start Docker service
 sudo systemctl start docker
 sudo systemctl enable docker
-sudo docker run -d -p 8080:8080 navchakravarthy/backendapi:latest 
 
+# Add user to Docker group (optional)
+sudo usermod -aG docker ubuntu
 
+# Pull and run a Docker container
+sudo docker run -d -p 8080:8080 navchakravarthy/backendapi:latest
