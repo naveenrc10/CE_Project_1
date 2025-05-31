8ffa26298b9e8500894483195404a3f3a8e157e0
Naveen R C <nav.chakravarthy@hgmail.com>
2025-05-31 05:30:13 +0000

    wip

--- aws_infra_setup/backendMS-init.sh
+++ aws_infra_setup/backendMS-init.sh
@@ -6,4 +6,4 @@ sudo apt-get install -y ansible unzip
 sudo snap install aws-cli --classic
 sudo snap install docker --classic
 
-sudo docker run -d -p 8080:8080 navchakravarthy/backendapi:latest
+sudo docker run -d -p 8080:8080 navchakravarthy/backendapi:latest 
