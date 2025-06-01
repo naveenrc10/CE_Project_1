796c231e6bc311b57047bdf213968e58beda9275
nchakravarthy <nav.chakravarthy@gmail.com>
2025-06-01 11:27:41 +0530

    wwww

--- backendAPI/src/main/java/com/iitk/courseend/project2/HelloController.java
+++ backendAPI/src/main/java/com/iitk/courseend/project2/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello, Spring Boot v1!";
+        return "Hello, Spring Boot v3!";
     }
 }
